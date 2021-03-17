57171f6434171c7f2f7af52e797ed6bb1312811b
Ahsan A <ahsan.a@allshorestaffing.com>
2021-03-18 00:33:04 +0500

    Removed authentication middleware for now To make all the eourted working

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -7,27 +7,6 @@ import { app } from "../../app";
 //import { startJob, stopJob } from "../services/job.service";
 
 const ProtectedRoutes: Router = Router();
-
-ProtectedRoutes.use((req, res, next) =>{
-    var token = req.headers['access-token'];
-    // decode token
-    if (token) {
-      // verifies secret and checks if the token is expired
-      jwt.verify(token.toString(), app.get('Secret'), (err:any, decoded:any) =>{      
-        if (err) {
-          return res.json({ message: 'invalid token' });    
-        } else {
-          // if everything is good, save to request for use in other routes
-          //req.decoded = decoded;    
-          next();
-        }
-      });
-    } else {
-      res.send({ 
-          message: 'No token provided.' 
-      });
-    }
-});
   
 ProtectedRoutes.get("/bikes", getBikes);
 ProtectedRoutes.post("/add-bike", addBike);
